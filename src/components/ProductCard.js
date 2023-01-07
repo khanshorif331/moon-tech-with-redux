387212a2cfb6c1e04f6b0fba63e5616fd2166211
khanshorif331 <khanshorif331@gmail.com>
2023-01-08 00:51:14 +0600

    remove from cart done

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { BiListPlus } from 'react-icons/bi'
 import { useDispatch } from 'react-redux'
-import { addToCart } from '../redux/actionCreators/productAction'
+import { useLocation } from 'react-router-dom'
+import {
+	addToCart,
+	removeFromCart,
+} from '../redux/actionCreators/productAction'
 
 const ProductCard = ({ product }) => {
 	const dispatch = useDispatch()
+	const { pathname } = useLocation()
 	return (
 		<div
 			className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
@@ -29,21 +34,32 @@ const ProductCard = ({ product }) => {
 				</ul>
 			</div>
 			<div className="flex gap-2 mt-5">
-				<button
-					onClick={() => dispatch(addToCart(product))}
-					className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
-				>
-					Add to cart
-				</button>
-				<button
-					title="Add to wishlist"
-					className="bg-indigo-500  py-1 px-2 rounded-full"
-				>
-					<BiListPlus className="text-white" />
-				</button>
+				{!pathname.includes('cart') && (
+					<button
+						onClick={() => dispatch(addToCart(product))}
+						className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+					>
+						Add to cart
+					</button>
+				)}
+				{pathname.includes('cart') && (
+					<button
+						onClick={() => dispatch(removeFromCart(product))}
+						className="bg-red-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+					>
+						Remove from cart
+					</button>
+				)}
+				{!pathname.includes('cart') && (
+					<button
+						title="Add to wishlist"
+						className="bg-indigo-500  py-1 px-2 rounded-full"
+					>
+						<BiListPlus className="text-white" />
+					</button>
+				)}
 			</div>
 		</div>
 	)
 }
-//
 export default ProductCard
